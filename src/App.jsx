6ec1bac612b11d96715d2fd9b2a40921aec459ec
Goodsigns <jojouna419@gmail.com>
2026-01-11 01:21:58 +0100

    removed the vite logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './sections/Home'
